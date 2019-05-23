4e4e0b368b7a0c51b227f5823c868a415b6a45ef
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-05-23 09:42:25 +0200

    added tests for checking runtime_config groups notifies

---
+++ spec/defines/g_docker_runtime_config_group_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'G_docker::Runtime_config::Group' do
+  let(:title) { 'test' }
+  let(:params) do
+    {
+      'container' => 'example',
+    }
+  end
+  let(:pre_condition) do
+    [
+      'class {"g_docker": data_vg_name => "vg_test", runtime_config_path => "/config.d" }',
+      'docker::run {"example": image => "example"}',
+      'g_docker::runtime_config {"example": reload_signal => "HUP"}',
+    ]
+  end
+
+  before(:each) do
+    Puppet::Parser::Functions.newfunction(:assert_private, type: :rvalue) { |args| }
+  end
+
+  context 'with source reload' do
+    let(:params) do
+      super().merge('source' => 'puppet://g_docker/test')
+    end
+
+    context 'disabled' do
+      let(:params) do
+        super().merge('source_reload' => false)
+      end
+
+      it { is_expected.to contain_file('/config.d/example/test').that_notifies('Docker::Run[example]') }
+    end
+
+    context 'enabled' do
+      let(:params) do
+        super().merge('source_reload' => true)
+      end
+
+      it { is_expected.to contain_file('/config.d/example/test').that_notifies('Exec[g_docker runtime config example]') }
+    end
+  end
+end
