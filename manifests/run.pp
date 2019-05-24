f20c47988afe50e850e1967e1f861ced4ce14b0c
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-05-24 08:45:23 +0200

    partially reverted 7f71faf14bbe4e621754bbcc930d49af4ed3bfde
    don't refresh Docker::Run when data/volumes change since we don't want
    to restart container when resizing volume

--- manifests/run.pp
+++ manifests/run.pp
@@ -202,7 +202,7 @@ define g_docker::run(
     ->Docker::Run[$name]
 
     G_docker::Data[$name]
-    ~>Docker::Run[$name]
+    ->Docker::Run[$name]
   } else {
     Docker::Run[$name]
     ->G_docker::Data[$name]
