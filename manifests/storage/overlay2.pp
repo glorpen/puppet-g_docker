763956ec45ff348230d374c1243bba2477ea5eea
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-06-07 07:55:29 +0200

    added support for lvmraid in overlay driver

--- manifests/storage/overlay2.pp
+++ manifests/storage/overlay2.pp
@@ -2,7 +2,10 @@ class g_docker::storage::overlay2(
   String $size,
   Enum['present', 'absent'] $ensure = 'present',
   Optional[String] $vg_name = undef,
-  String $lv_name = 'docker-data'
+  String $lv_name = 'docker-data',
+  Optional[Integer] $raid_level = undef,
+  Optional[Integer] $raid_stripes = undef,
+  Optional[Integer] $raid_mirrors = undef
 ){
   if $ensure == 'present' {
     class {'g_docker::storage':
--- manifests/storage/overlay2_helper.pp
+++ manifests/storage/overlay2_helper.pp
@@ -7,18 +7,37 @@ class g_docker::storage::overlay2_helper{
   $lv_name = $::g_docker::storage::overlay2::lv_name
   $size = $::g_docker::storage::overlay2::size
 
-  if $ensure == 'present' {
-    G_server::Volumes::Vol[$lv_name] -> Class['docker']
+  if ($::g_docker::storage::overlay2::raid_level == undef) {
+    $vol_resource = G_server::Volumes::Vol[$lv_name]
   } else {
-    Class['docker'] -> G_server::Volumes::Vol[$lv_name]
+    $vol_resource = G_server::Volumes::Raid[$lv_name]
   }
 
-  g_server::volumes::vol { $lv_name:
+  if $ensure == 'present' {
+    $vol_resource -> Class['docker']
+  } else {
+    Class['docker'] -> $vol_resource
+  }
+
+  $vol_options = {
     ensure     => $ensure,
     vg_name    => $vg_name,
     size       => $size,
     fs         => 'xfs',
     fs_options => '-n ftype=1',
     mountpoint => $::g_docker::docker_data_path
+  }
+
+  if ($::g_docker::storage::overlay2::raid_level == undef) {
+    g_server::volumes::vol { $lv_name:
+      * => $vol_options
+    }
+  } else {
+    g_server::volumes::raid { $lv_name:
+      level   => $::g_docker::storage::overlay2::raid_level,
+      stripes => $::g_docker::storage::overlay2::raid_stripes,
+      mirrors => $::g_docker::storage::overlay2::raid_mirrors,
+      *       => $vol_options,
+    }
   }
 }
