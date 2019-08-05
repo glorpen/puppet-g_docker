ada4892ebfd43b5964a3a02396bceafe1c6df440
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-08-05 20:04:17 +0200

    gate rules for external access

--- manifests/firewall/puppet_network.pp
+++ manifests/firewall/puppet_network.pp
@@ -6,15 +6,6 @@ define g_docker::firewall::puppet_network(
   $iface = g_docker::find_network_interface($name)
 
   if $iface {
-    g_firewall::ipv4 { "150 docker bridge ${iface} allow estabilished connections":
-      ensure   => $ensure,
-      chain    => 'FORWARD',
-      outiface => $iface,
-      proto    => 'all',
-      action   => 'accept',
-      ctstate  => ['RELATED', 'ESTABLISHED']
-    }
-
     g_firewall::ipv4 { "150 docker bridge ${iface} allow forwarding to itself":
       ensure   => $ensure,
       chain    => 'FORWARD',
@@ -24,27 +15,38 @@ define g_docker::firewall::puppet_network(
       action   => 'accept',
     }
 
-    g_server::get_interfaces('external').each | $ex_iface | {
-      g_firewall::ipv4 { "150 docker bridge ${iface} allow forwarding to ${ex_iface}":
+    if ($external_access) {
+      g_firewall::ipv4 { "150 docker bridge ${iface} allow estabilished connections":
         ensure   => $ensure,
         chain    => 'FORWARD',
-        iniface  => $iface,
-        outiface => $ex_iface,
+        outiface => $iface,
         proto    => 'all',
         action   => 'accept',
+        ctstate  => ['RELATED', 'ESTABLISHED']
       }
 
-      $config['ipam']['config'].each | $ipam | {
-        $subnet = $ipam['subnet']
-        if $subnet =~ Stdlib::IP::Address::V4::CIDR {
-          g_firewall::ipv4 { "150 docker bridge ${iface} nat to ${ex_iface}":
-            ensure   => $ensure,
-            table    => 'nat',
-            chain    => 'POSTROUTING',
-            source   => $subnet,
-            outiface => $ex_iface,
-            proto    => 'all',
-            jump     => 'MASQUERADE',
+      g_server::get_interfaces('external').each | $ex_iface | {
+        g_firewall::ipv4 { "150 docker bridge ${iface} allow forwarding to ${ex_iface}":
+          ensure   => $ensure,
+          chain    => 'FORWARD',
+          iniface  => $iface,
+          outiface => $ex_iface,
+          proto    => 'all',
+          action   => 'accept',
+        }
+
+        $config['ipam']['config'].each | $ipam | {
+          $subnet = $ipam['subnet']
+          if $subnet =~ Stdlib::IP::Address::V4::CIDR {
+            g_firewall::ipv4 { "150 docker bridge ${iface} nat to ${ex_iface}":
+              ensure   => $ensure,
+              table    => 'nat',
+              chain    => 'POSTROUTING',
+              source   => $subnet,
+              outiface => $ex_iface,
+              proto    => 'all',
+              jump     => 'MASQUERADE',
+            }
           }
         }
       }
