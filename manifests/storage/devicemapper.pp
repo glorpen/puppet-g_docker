0b355d8b332f97bcb1ea8eb3cd041915dd699d7d
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-11-27 10:53:24 +0100

    fixed devicemapper default vg_name handling

--- manifests/storage/devicemapper.pp
+++ manifests/storage/devicemapper.pp
@@ -13,25 +13,21 @@
 #
 class g_docker::storage::devicemapper(
   String $basesize,
+  String $vg_name,
   String $thinpool_size,
   String $thinpool_metadata_size,
   String $thinpool_name = 'docker-thin',
-  Optional[String] $vg_name = undef,
   Enum['present', 'absent'] $ensure = 'present'
 ){
   $_vol_name = regsubst($thinpool_name, '-', '--', 'G')
   $_vg_name = regsubst($vg_name, '-', '--', 'G')
 
   if $ensure == 'present' {
-    $_storage_vg = $vg_name?{
-      undef   => $::g_docker::data_vg_name,
-      default => $vg_name
-    }
     class {'g_docker::storage':
       docker_config => {
         storage_driver => 'devicemapper',
         dm_basesize    => $basesize,
-        storage_vg     => $_storage_vg,
+        storage_vg     => $_vg_name,
         dm_thinpooldev => "/dev/mapper/${_vg_name}-${_vol_name}",
         dm_blkdiscard  => true,
       }
@@ -45,7 +41,7 @@ class g_docker::storage::devicemapper(
   # /var/lib/docker -> mostly for volumes data
   g_server::volumes::thinpool { $thinpool_name:
     ensure        => $ensure,
-    vg_name       => $vg_name,
+    vg_name       => $_vg_name,
     size          => $thinpool_size,
     metadata_size => $thinpool_metadata_size
   }
