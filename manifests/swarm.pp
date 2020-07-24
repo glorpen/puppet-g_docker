5256c345eaa68083aebdd150383e9b0869650c07
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2020-07-24 08:00:58 +0200

    added firewall rules based on exported resources

--- manifests/swarm.pp
+++ manifests/swarm.pp
@@ -1,9 +1,21 @@
+# @summary Configures Docker Swarm
+#
+# @param cluster_iface
+#   Interface that will be used to communicate with other nodes.
+# @param manager_ip
+#   Address of existing manager to connect to when initializing.
+# @param token
+#   Token to use when joining swarm.
+# @param firewall_mode
+#   Open swarm ports to whole cluster_iface ('interface') or for each other swarm node separately ('node').
+#
 class g_docker::swarm(
   String $cluster_iface,
   Optional[String] $manager_ip = undef,
   Optional[String] $token = undef,
   String $node_name = $::fqdn,
   Array[Tuple[Stdlib::IP::Address::V4::CIDR, Integer]] $address_pools = [],
+  Enum['interface', 'node'] $firewall_mode = 'interface',
 ){
   include ::g_docker
 
@@ -52,16 +64,49 @@ class g_docker::swarm(
     *              => $_swarm_opts + $_swarm_pool_opts
   }
 
-  g_firewall { '105 allow inbound docker swarm tcp':
-    dport   => [2377, 7946],
-    proto   => tcp,
-    action  => accept,
-    iniface => $cluster_iface
+  $_firewall_rules = {
+    'udp' => {
+        'dport'  => [2377, 7946],
+        'proto'  => tcp,
+        'action' => accept,
+    },
+    'tcp' => {
+      'dport'  => [4789, 7946],
+      'proto'  => udp,
+      'action' => accept,
+    }
   }
-  g_firewall { '105 allow inbound docker swarm udp':
-    dport   => [4789, 7946],
-    proto   => udp,
-    action  => accept,
-    iniface => $cluster_iface
+
+  case $firewall_mode {
+    'interface': {
+      $_firewall_rules.each | $proto, $config | {
+        g_firewall { "105 allow inbound docker swarm ${proto}":
+          iniface => $cluster_iface,
+          *       => $config
+        }
+      }
+    }
+    'node': {
+      $_firewall_rules.each | $proto, $rule_config | {
+        $rule_name = "105 allow inbound docker swarm ${proto} from ${node_name}"
+        $config = merge($rule_config, {
+          source => $_listen_addr,
+          tag    => ['g_docker::swarm::node'],
+        })
+        if $_listen_addr =~ Stdlib::IP::Address::V6 {
+          @@g_firewall::ipv6 { $rule_name: * => $config}
+        } else {
+          @@g_firewall::ipv4 { $rule_name: * => $config}
+        }
+      }
+
+      G_firewall::Ipv4 <<| tag == 'g_docker::swarm::node' |>> {
+        iniface => $cluster_iface
+      }
+      G_firewall::Ipv6 <<| tag == 'g_docker::swarm::node' |>> {
+        iniface => $cluster_iface
+      }
+    }
+    default: {}
   }
 }
